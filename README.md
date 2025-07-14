e5f14f9247232b66331832b821496b9416ed3078
oussamanmh <mo_nemamcha@esi.dz>
2025-07-14 19:45:54 +0200

    randomness stuff

--- README.md
+++ README.md
@@ -26,3 +26,22 @@ function transfer(address to, uint256 amount) public {
     emit Transfer(msg.sender, to, amount);
 }
 ```
+
+## Randomness in Blockchain
+
+In blockchain, randomness is crucial for applications like lotteries, games, and other scenarios where unpredictable outcomes are required. However, achieving true randomness in a deterministic environment like a blockchain can be challenging.
+
+### Challenges of Randomness in Blockchain
+
+1. **Determinism**: Every node in the network must arrive at the same state, making it difficult to generate random numbers that are not predictable.
+2. **Security**: Randomness must be secure against manipulation by malicious actors.
+3. **Consensus**: Randomness must be
+   consistent across all nodes to ensure fairness and integrity.
+
+### Solutions for Randomness
+
+1. **Chainlink VRF (Verifiable Random Function)**: A decentralized oracle service that provides provably random numbers.
+
+### Virtuel Key word in Solidity
+
+The `Virtuel` keyword in Solidity is used to define functions that can be overridden by derived contracts
--- contracts/raffle.sol
+++ contracts/raffle.sol
@@ -2,21 +2,43 @@
 
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity 0.8.7;
-
+import "@chainlink/contracts/src/v0.8/vrf/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
 error Raffle__NotEnoughEthEntered();
+error Raffle__TransferFailed();
 
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2 {
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    bytes32 private immutable i_gasLine;
+    uint64 private immutable i_subscriptionId;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private immutable i_callbackGasLimit;
+    uint32 private constant NUM_WORDS = 1;
+
+    address private s_recentWinner;
 
     /*Events*/
     event RaffleEnter(address indexed player);
+    event RequestedRaffleWinner(uint256 indexed requestId);
+    event WinnerPicked(address indexed winner);
 
-    constructor(uint256 _entranceFee) {
+    constructor(
+        address vrfCoordinatorV2,
+        uint256 _entranceFee,
+        bytes32 gasLine,
+        uint64 subscriptionId,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_entranceFee = _entranceFee;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
+        i_gasLine = gasLine;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
     }
 
-    function enterRaffle(uint256 _entranceFee) public payable {
+    function enterRaffle() public payable {
         if (msg.value < i_entranceFee) {
             revert Raffle__NotEnoughEthEntered();
         }
@@ -24,13 +46,43 @@ contract Raffle {
         emit RaffleEnter(msg.sender);
     }
 
+    function requestRandomWinner() external {
+        //request the random number
+        // Once we get it . do somthing with it
+        // 2 transactions process
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLine, //gasline
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
+        emit RequestedRaffleWinner(requestId);
+    }
+
+    function fulfillRandomWords(
+        uint256 /*requestId*/,
+        uint256[] memory randomwords
+    ) internal override {
+        uint256 indexOfWinner = randomwords[0] % s_players.length;
+        address payable recentWinner = s_players[indexOfWinner];
+        s_recentWinner = recentWinner;
+        (bool success, ) = recentWinner.call{value: address(this).balance}("");
+        if (!success) {
+            revert Raffle__TransferFailed();
+        }
+        emit WinnerPicked(recentWinner);
+    }
+
     function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
     }
 
-    function getPlayers(
-        uint256 index
-    ) public view returns (address payable[] memory) {
+    function getPlayer(uint256 index) public view returns (address) {
         return s_players[index];
+    }
+
+    function getRecentWinner() public view returns (address) {
+        return s_recentWinner;
     }
 }
